57114558105978ec2e3619ad4c6fe4ca1727af71
Marcin Brzeziński <xbrzezinx@gmail.com>
2019-01-24 14:25:35 +0100

    Polimorfizm

--- cwiczenia/Polimorfizm/polimorfizm.py
+++ cwiczenia/Polimorfizm/polimorfizm.py
@@ -8,8 +8,8 @@ class Zwierze(object):
         self.move_y = y + 1
         return self.move_x, self.move_y
 
-    def wyswietl(self):
-        print("Poruszam sie: x: " + str(self.move_x) + " oraz y: " + str(self.move_y))
+    def wyswietl(self, imie):
+        print("Jestem " + str(imie) + ", poruszam sie: x: " + str(self.move_x) + " oraz y: " + str(self.move_y))
 
 
 class Ssak(Zwierze):
@@ -17,8 +17,9 @@ class Ssak(Zwierze):
         self.idz = Zwierze.poruszaj_sie(self, x, y)
         self.idz_x, self.idz_y = self.idz
 
-    def wyswietl(self):
-        print("Idę: x: " + str(self.idz_x) + " oraz y: " + str(self.idz_y))
+    def wyswietl(self, imie):
+        print("Jestem " + str(imie) + ", idę: x: " + str(self.idz_x) + " oraz y: " + str(self.idz_y))
+
 
 
 class Ryba(Zwierze):
@@ -26,8 +27,8 @@ class Ryba(Zwierze):
         self.plyn = Zwierze.poruszaj_sie(self, x, y)
         self.plyn_x, self.plyn_y = self.plyn
 
-    def wyswietl(self):
-        print("Płynę: x: " + str(self.plyn_x) + " oraz y: " + str(self.plyn_y))
+    def wyswietl(self, imie):
+        print("Jestem " + str(imie) + ", płynę: x: " + str(self.plyn_x) + " oraz y: " + str(self.plyn_y))
 
 
 class Ptak(Zwierze):
@@ -35,18 +36,21 @@ class Ptak(Zwierze):
         self.lec = Zwierze.poruszaj_sie(self, x, y)
         self.lec_x, self.lec_y = self.lec
 
-    def wyswietl(self):
-        print("Lecę: x: " + str(self.lec_x) + " oraz y: " + str(self.lec_y))
+    def wyswietl(self, imie):
+        print("Jestem " + str(imie) + ", lecę: x: " + str(self.lec_x) + " oraz y: " + str(self.lec_y))
 
 
 def main():
     zwierzeta = [Zwierze(), Ssak(), Ryba(), Ptak()]
     x = 3
     y = 4
+    i = 0
     print("x = " + str(x), " y = " + str(y))
+    imiona = ["zwierzęciem", "ssakiem", "rybą", "ptakiem"]
     for zwierze in zwierzeta:
         zwierze.poruszaj_sie(x, y)
-        zwierze.wyswietl()
+        zwierze.wyswietl(imiona[i])
+        i += 1
         x = x ** 2
         y = y ** 2
 
