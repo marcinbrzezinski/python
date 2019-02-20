3d9dc312c4678fdd13273b64636cd045c1b5b6bb
Marcin Brzeziński <xbrzezinx@gmail.com>
2019-02-20 12:45:20 +0100

    Wizualizacja

--- cwiczenia/Szereg_czasowy/main.py
+++ cwiczenia/Szereg_czasowy/main.py
@@ -1,13 +1,25 @@
 import pandas as pd
 from dateutil import parser
 import matplotlib.pyplot as plt
+import numpy as np
 
 df = pd.read_csv('~/Pulpit/prophet/examples/example_yosemite_temps.csv')
 
-df['Datetime'] = pd.to_datetime(df['ds'], format = "%Y-%m-%d %H:%M:%S")
-df['Day'] = df['Datetime'].apply(lambda x: x.day)
+df['Datetime'] = pd.to_datetime(df['ds'], format="%Y-%m-%d %H:%M:%S")
+df['Hour'] = df['Datetime'].apply(lambda x: x.hour)
+df['Weekday'] = df['Datetime'].apply(lambda x: x.weekday())
 
-data = df.groupby(['Day'])['y'].agg('mean')
+data = df.groupby(['Weekday', 'Hour'])['y'].agg('mean').reset_index()
 
+plt.imshow(np.array(pd.pivot_table(data, values='y',
+                                   index='Weekday',
+                                   columns='Hour').fillna(0)
+                    ),
+           cmap='coolwarm',
+           label='Cout',
+           interpolation='spline36',
+           aspect='auto'
+           )
 
-print(data)
+# print(data)
+plt.show()
