704fbc4e99f7e381711e0d3bbd369bb188daf384
Marcin Brzeziński <xbrzezinx@gmail.com>
2019-02-20 13:09:06 +0100

    Update

--- cwiczenia/Szereg_czasowy/prophet.py
+++ cwiczenia/Szereg_czasowy/prophet.py
@@ -4,10 +4,10 @@ from fbprophet import Prophet
 
 df = pd.read_csv('~/Pulpit/prophet/examples/example_yosemite_temps.csv')
 
-m = Prophet()
+m = Prophet(changepoint_prior_scale=0.01)
 m.fit(df)
 
-future = m.make_future_dataframe(periods=365)
+future = m.make_future_dataframe(periods=10)
 # future.tail()     - wyswietla 5 ostatnich wartości
 
 forecast = m.predict(future)
