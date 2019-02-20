0036ddffbc68883588cfa99a7c28a4215905a8e5
Marcin Brzeziński <xbrzezinx@gmail.com>
2019-02-20 12:30:26 +0100

    Wypisanie średeniej temperatury w poszczególnych dniach miesiąca (1-31)

--- cwiczenia/Szereg_czasowy/main.py
+++ cwiczenia/Szereg_czasowy/main.py
@@ -4,13 +4,10 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv('~/Pulpit/prophet/examples/example_yosemite_temps.csv')
 
-df['Date'] = df['ds'].apply(lambda x: parser.parse(x).month)
+df['Datetime'] = pd.to_datetime(df['ds'], format = "%Y-%m-%d %H:%M:%S")
+df['Day'] = df['Datetime'].apply(lambda x: x.day)
 
-data = df.groupby(['Date'])['y'].agg('mean')        # mean - średnia
-
-ax = data.plot(color='blue')
-
-plt.show()
+data = df.groupby(['Day'])['y'].agg('mean')
 
 
 print(data)
